6414200f045a99ed88bf870380eab8c4ad3392d6
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2026-01-21 16:56:11 +0800

    refactor

--- library.js
+++ library.js
@@ -1,19 +1,40 @@
 const dialog = document.querySelector("dialog");
 const addBook = document.getElementById("addbook");
 const closeButton = document.getElementById("closeButton");
-
 const cardContainer = document.getElementById("cardContainer");
 
 const myLibrary = [];
 
-function Book(title, author, pages, readStatus, id) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
-    this.id = id;
+const dataBook = [
+    { label: 'Title: ', key: 'title' },
+    { label: 'Author: ', key: 'author' },
+    { label: 'Pages: ', key: 'pages' },
+    { label: 'Read Status: ', key: 'readStatus' },
+    { label: 'ID: ', key: 'id' }
+
+];
+
+class Book {
+    constructor(title, author, pages, readStatus, id) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+        this.id = id;
+    }
+
+    readStatusToggle() {
+        if (this.readStatus === 'Read') {
+            this.readStatus = 'Not Read';
+        }
+        else {
+            this.readStatus = 'Read';
+        }
+    }
 
 }
+
+
 
 function addBookToLibrary(titles, author, pages, readStatus) {
 
@@ -23,19 +44,9 @@ function addBookToLibrary(titles, author, pages, readStatus) {
 }
 
 
-
 function loopLibrary() {
     cardContainer.innerHTML = '';
     for (let i = 0; i < myLibrary.length; i++) {
-
-        const dataBook = [
-            { label: 'Title: ', key: 'title' },
-            { label: 'Author: ', key: 'author' },
-            { label: 'Pages: ', key: 'pages' },
-            { label: 'Read Status: ', key: 'readStatus' },
-            { label: 'ID: ', key: 'id' }
-
-        ];
 
         const card = document.createElement('div');
         card.style.width = '20%';
@@ -48,7 +59,6 @@ function loopLibrary() {
         removeButton.textContent = 'Remove Book';
         removeButton.style.marginTop = '1rem';
         removeButton.setAttribute('data-id', myLibrary[i].id);
-
         removeButton.addEventListener('click', () => {
             const bookId = removeButton.getAttribute('data-id');
             const bookIndex = myLibrary.findIndex(book => book.id === bookId);
@@ -66,7 +76,7 @@ function loopLibrary() {
         toggleButton.addEventListener('click', () => {
             const bookId = toggleButton.getAttribute('data-id');
             const bookIndex = myLibrary.findIndex(book => book.id === bookId);
-            myLibrary[bookIndex].toggleReadStatus();
+            myLibrary[bookIndex].readStatusToggle();
             loopLibrary();
         });
 
@@ -74,33 +84,36 @@ function loopLibrary() {
         dataBook.forEach((element) => {
             const cardContent = document.createElement('div')
             cardContent.textContent = element.label + myLibrary[i][element.key];
-            
-            if (myLibrary[i].readStatus === 'Read'){
-                card.style.backgroundColor = 'lightgreen';
-            }
-            else {
-                card.style.backgroundColor = 'red';
-            }
-            
             card.appendChild(cardContent);
-            cardContainer.appendChild(card);
-            card.appendChild(removeButton);
-            card.appendChild(toggleButton);
         });
+
+        if (myLibrary[i].readStatus === 'Read') {
+            card.style.backgroundColor = 'lightgreen';
+        }
+        else {
+            card.style.backgroundColor = 'red';
+        }
+        
+        card.appendChild(removeButton);
+        card.appendChild(toggleButton);
+        cardContainer.appendChild(card);
     }
 }
 
-Book.prototype.toggleReadStatus = function () {
-    if (this.readStatus === 'Read') {
-        this.readStatus = 'Not Read';
-    }
-    else {
-        this.readStatus = 'Read';
-    }
-}
 
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", (e) => {
+    const getTitle = document.getElementById("titleName").value;
+    const getAuthor = document.getElementById("authorName").value;
+    const getPage = document.getElementById("numberAmount").value;
+    const readStatus = document.querySelector('input[name="readStatus"]:checked').value;
 
+    addBookToLibrary(getTitle, getAuthor, getPage, readStatus);
+    loopLibrary();
+    e.preventDefault();
 
+});
+// "Add Book" button opens the dialog
 addBook.addEventListener("click", () => {
     dialog.showModal();
 });
@@ -108,22 +121,4 @@ addBook.addEventListener("click", () => {
 // "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
     dialog.close();
-});
-
-
-const submitButton = document.getElementById("submitButton");
-
-submitButton.addEventListener("click", (e) => {
-
-
-    const getTitle = document.getElementById("titleName").value;
-    const getAuthor = document.getElementById("authorName").value;
-    const getPage = document.getElementById("numberAmount").value;
-    const readStatus = document.querySelector('input[name="readStatus"]:checked').value;
-
-
-    addBookToLibrary(getTitle, getAuthor, getPage, readStatus);
-    loopLibrary();
-    e.preventDefault();
-
 });
